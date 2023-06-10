93b8ea268c23ca67f111b4cc27ebe536c6c6b621
Haik <haiksarg111@gmail.com>
2023-06-10 16:14:34 +0400

    _1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,7 +76,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', 'postgres'),
+            'NAME': os.getenv('DB_NAME', 'foodgram'),
             'USER': os.getenv('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'progpass'),
             'HOST': os.getenv('DB_HOST', 'db'),
